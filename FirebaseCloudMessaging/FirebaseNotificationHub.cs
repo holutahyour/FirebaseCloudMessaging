f579000c0aaab6ad7c58f7f3abe70386dccd5e42
Victor Onibudo <holutahyour@users.noreply.github.com>
2024-03-22 20:51:18 +0100

    init

---
+++ FirebaseCloudMessaging/FirebaseNotificationHub.cs
@@ -0,0 +1,71 @@
+﻿using FirebaseAdmin.Messaging;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+
+namespace FirebaseCloudMessaging;
+public class FirebaseNotificationHub
+{
+    public FirebaseNotificationHub()
+    {
+        FirebaseApp.Create(new AppOptions()
+        {
+            Credential = GoogleCredential.FromFile("private_key.json")
+        });
+    }
+
+    public void SendNotifcationToTopic(string title, string body, string topic = "all")
+    {        
+
+        var message = new Message()
+        {
+            Topic = topic,
+            Notification = new Notification()
+            {
+                Title = title,
+                Body = body,
+            }
+        };
+
+        string response = FirebaseMessaging.DefaultInstance.SendAsync(message).Result;
+
+        // Response is a message ID string.
+        Console.WriteLine("Successfully sent message: " + response);
+    }
+
+    public void SendNotifcationToDevice(string title, string body, string token)
+    {
+
+        var message = new Message()
+        {
+            Token = token,
+            Notification = new Notification()
+            {
+                Title = title,
+                Body = body
+            }
+        };
+
+        string response = FirebaseMessaging.DefaultInstance.SendAsync(message).Result;
+
+        // Response is a message ID string.
+        Console.WriteLine("Successfully sent message: " + response);
+    }
+
+    public void SendNotifcationToMultipleDevices(string title, string body, string[] tokens)
+    {
+        var message = new MulticastMessage()
+        {
+            Tokens = tokens,
+            Notification = new Notification()
+            {
+                Title = title,
+                Body = body
+            }
+        };
+
+        BatchResponse response = FirebaseMessaging.DefaultInstance.SendMulticastAsync(message).Result;
+
+        // Response is a message ID string.
+        Console.WriteLine("Successfully sent message: " + response);
+    }
+}
---
+++ FirebaseCloudMessaging/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FirebaseCloudMessaging;
+
+// This registration token comes from the client FCM SDKs.
+string token = "TOKEN_HERE";
+
+
+// This registration tokens comes from the client FCM SDKs.
+string[] tokens = new List<string>()
+{
+    "TOKEN_HERE",
+    "TOKEN_HERE",
+    "TOKEN_HERE"
+}.ToArray();
+
+
+// The message the user will recieve
+string title = "Test from code";
+string body = "Here is your test!";
+
+var fcm = new FirebaseNotificationHub();
+
+//fcm.SendNotifcationToDevice(title, body, token);
+//fcm.SendNotifcationToMultipleDevices(title, body, tokens);
+fcm.SendNotifcationToTopic(title, body);
